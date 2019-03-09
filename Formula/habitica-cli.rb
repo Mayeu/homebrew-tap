55011a87b39c5de9ecf47d61276ef7040fe24f26
Mayeu <m@mayeu.me>
2019-03-09 12:14:03 +0700

    feat(habitica-cli): bump to v0.0.2

--- Formula/habitica-cli.rb
+++ Formula/habitica-cli.rb
@@ -1,8 +1,9 @@
 class HabiticaCli < Formula
   desc "A ⚠️proof-of-concept⚠️ of an Habitica CLI"
+  version "0.0.2"
   homepage ""
-  url "https://github.com/Mayeu/habitica-cli/archive/v0.0.1.tar.gz"
-  sha256 "5fc587f3b77d0cbe349068671146de75ee569a409bdaf33c16dc4b5392895746"
+  url "https://github.com/Mayeu/habitica-cli/archive/v0.0.2.tar.gz"
+  sha256 "dec2b8609e098149ef6a208e73104a93347b77ba064c55e14c231c14da277057"
 
   def install
     bin.install "habitica"
