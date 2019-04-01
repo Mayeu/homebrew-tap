35d2d339bde783fd5e8f7331f25dbc44b1b4b313
Mayeu <m@mayeu.me>
2019-04-01 10:20:35 +0700

    feat(habitica-cli): 0.0.2 -> 0.1.0

--- Formula/habitica-cli.rb
+++ Formula/habitica-cli.rb
@@ -1,9 +1,9 @@
 class HabiticaCli < Formula
   desc "A ⚠️proof-of-concept⚠️ of an Habitica CLI"
-  version "0.0.2"
+  version "0.1.0"
   homepage ""
-  url "https://github.com/Mayeu/habitica-cli/archive/v0.0.2.tar.gz"
-  sha256 "dec2b8609e098149ef6a208e73104a93347b77ba064c55e14c231c14da277057"
+  url "https://github.com/Mayeu/habitica-cli/archive/v0.1.0.tar.gz"
+  sha256 "01be7e2f54db7a56ff71ef7931d90ba9a7ee663c77c0d42238a30b7dcae5895f"
 
   def install
     bin.install "habitica"
